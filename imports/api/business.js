272c34e1f8154b457f804f5a941ad9592010b652
sanfordcb <sanford0518@gmail.com>
2016-09-15 09:43:42 -0500

    Implement method for API call to update downtimes in Business collection

--- imports/api/business.js
+++ imports/api/business.js
@@ -1,9 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+import { HTTP } from 'meteor/http';
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-var updownKey = process.env.UPDOWN_KEY;
+const updownKey = Meteor.settings.updownKey;
 
 export const Business = new Mongo.Collection('business');
+
+if(Meteor.isServer) {
+  Meteor.publish('business', function businessPublication() {
+    return Business.find();
+  });
+}
+
+Meteor.methods({
+  'retrieveDowntimes'() {
+    Business.find().forEach((biz) => {
+      let tokenId = biz.updown_token;
+      let url = "http://updown.io/api/checks/" + tokenId;
+      url += "/downtimes?api-key=" + updownKey;
+      HTTP.call('GET',url, (error, result) => {
+        if(!error) {
+          let data = result.data;
+          Business.update(biz, { $set: { downtimes: data } });
+        }
+      });
+    });
+  }
+});
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Business } from '../api/business.js';
@@ -6,19 +7,24 @@ import { Business } from '../api/business.js';
 import TimelineWebsiteUptime from './TimelineWebsiteUptime.jsx';
 
 class App extends Component {
+  componentDidMount() {
+    Meteor.call('retrieveDowntimes');
+  }
+  buildTimelines() {
+    console.log(this.props.business);
+  }
   // Add method to iterate through Business collection
   // Create TimelineWebsiteUptime component w/ business name
   // Pass outage data via props to be used by TimelineTick
 
   render() {
-
-    console.log(this.props.business);
     return (
       <div className="container">
         <header>
           <h1>Website Uptime Timeline</h1>
         </header>
         <TimelineWebsiteUptime />
+        {this.buildTimelines()}
       </div>
     );
   }
@@ -29,6 +35,8 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
+  Meteor.subscribe('business');
+
   return {
     business: Business.find({}).fetch()
   };
