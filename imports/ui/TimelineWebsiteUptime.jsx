4026a1190c3341ca36931c709a01fdf32f891d51
sanfordcb <sanford0518@gmail.com>
2016-09-15 10:43:22 -0500

    Start writing method for generating TimeTick components

--- imports/ui/TimelineWebsiteUptime.jsx
+++ imports/ui/TimelineWebsiteUptime.jsx
@@ -5,6 +5,15 @@ import TimeTick from './TimeTick.jsx';
 export default class TimelineWebsiteUptime extends Component {
   // Receive TimeTick data via props
   // Iterate through TimeTick data, to generate TimeTicks
+  generateTimeTicks() {
+    // iterate through downtimes to get errors
+    // use dateTracker obj to avoid repeating errors for days
+    
+    const dateTracker = {};
+    this.props.downtimes.map((check) => {
+      let checkDay = check.started_at.slice(0, 9);
+    });
+  }
 
   render() {
     return (
